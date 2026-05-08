cd7629bc915e30fa780ca36e5f365bc365a7848c
Sonam Narula <sonamnarula2108@gmail.com>
2026-05-08 16:47:20 +0530

    Add odd number square pattern

--- LEARNING PATTERS/OddNumberTriangle.cpp
+++ LEARNING PATTERS/OddNumberTriangle.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main() {
     for (int rows = 1; rows <= 5; rows++) {
-        int num = 1;  // har row me reset hoga
+        int num = 1;  // har row me reset
 
         for (int columns = 1; columns <= rows; columns++) {
             cout << num << " ";
-            num += 2;  // next odd number
+            num += 2;
         }
 
         cout << endl;
