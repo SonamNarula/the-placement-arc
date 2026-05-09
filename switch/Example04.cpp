224b8be2ac81a2c2a914ba72cd618bf6ca71638e
Sonam Narula <sonamnarula2108@gmail.com>
2026-05-09 18:44:11 +0530

    Add array example and CBT 8

--- switch/Example04.cpp
+++ switch/Example04.cpp
@@ -1,19 +1,26 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
-					
-	char gear = 'R';
-	
-	switch (gear) {
-	    case 'P':	cout << "Park – Car Locked\n"; break;
-	    case 'R':	cout << "Reverse – Moving Backward\n"; break;
-	    case 'N':	cout << "Neutral – No Power to Wheels\n"; break;
-	    case 'D':	cout << "Drive – Moving Forward\n"; break;
-	    default:	cout << "Unknown Gear ??? \n";
-	}	
+int main() {
+    char gear = 'R';
 
-	return 0;
+    switch (gear) {
+        case 'P':
+            cout << "Park – Car Locked\n";
+            break;
+        case 'R':
+            cout << "Reverse – Moving Backward\n";
+            break;
+        case 'N':
+            cout << "Neutral – No Power to Wheels\n";
+            break;
+        case 'D':
+            cout << "Drive – Moving Forward\n";
+            break;
+        default:
+            cout << "Unknown Gear ??? \n";
+    }
 
+    return 0;
 }
